220ef404aca8023c6f88077743cc13cd296c19d0
mj-e-boc <92397843+mj-e-boc@users.noreply.github.com>
2022-07-19 13:24:53 +0200

    add index.html file for menu

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const hamburger = document.querySelector('.hamburger');
-const navmenu = document.querySelector('.nav-menu');
+const hamburger = document.querySelector('#hamburger');
+const navmenu = document.querySelector('#nav-menu');
 
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navmenu.classList.toggle('active');
 })
+
+document.querySelectorAll('.nav-linki').forEach(n => n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navmenu.classList.remove('active');
+}))
